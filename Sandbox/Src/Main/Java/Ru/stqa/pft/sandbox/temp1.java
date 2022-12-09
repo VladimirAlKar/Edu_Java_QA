ec34904dd9abcab3d30bade31b5c387ece3bcfda
Karachkov Vladimir <vladimir.karachkov@dataart.com>
2022-12-09 17:27:14 +0300

    Изменен codstyles в IDEA

--- Sandbox/Src/Main/Java/Ru/stqa/pft/sandbox/temp1.java
+++ Sandbox/Src/Main/Java/Ru/stqa/pft/sandbox/temp1.java
@@ -1,8 +1,8 @@
 package Ru.stqa.pft.sandbox;
 
 public class temp1 {
-public static void main (String[] args) {
+  public static void main(String[] args) {
 
-System.out.println ("Hello, World!");
-}
+    System.out.println("Hello, World!");
+  }
 }
